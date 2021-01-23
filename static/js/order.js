e92e8f997eb175c8bbc2c4aa4158f849cc77d59b
ChanceDurr <chancedurr@gmail.com>
2021-01-22 21:49:37 -0800

    added some functionality to the order buttons and working on a pizza list with prices and totals

--- static/js/order.js
+++ static/js/order.js
@@ -26,8 +26,8 @@ const sizes = ["10", "12", "14"]
 $('#sauce_choices').html(
     $.map(toppings.sauces, function(val,i) {
         return $('<div/>', { class: 'form-check form-check-inline' }).html([
-            $('<input/>', { class: "btn-check", type: "checkbox", id: "sauceCheckbox"+i, value: val }),
-            $('<label/>', { class: "btn btn-primary topping", for: "sauceCheckBox"+i }).text(val)]
+            $('<input/>', { class: "btn-check", type: "checkbox", autocomplete: "off", id: "sauceCheckbox"+i, value: val }),
+            $('<label/>', { class: "btn btn-outline-primary topping", for: "sauceCheckBox"+i }).text(val)]
         )
       
     })
@@ -36,8 +36,8 @@ $('#sauce_choices').html(
 $('#cheese_choices').html(
     $.map(toppings.cheeses, function(val,i) {
         return $('<div/>', { class: 'form-check form-check-inline' }).html([
-            $('<input/>', { class: "btn-check", type: "checkbox", id: "cheeseCheckbox"+i, value: val }),
-            $('<label/>', { class: "btn btn-primary topping", for: "cheeseCheckBox"+i }).text(val)]
+            $('<input/>', { class: "btn-check", type: "checkbox", autocomplete: "off", id: "cheeseCheckbox"+i, value: val }),
+            $('<label/>', { class: "btn btn-outline-primary topping", for: "cheeseCheckBox"+i }).text(val)]
         )
       
     })
@@ -46,8 +46,8 @@ $('#cheese_choices').html(
 $('#meat_choices').html(
     $.map(toppings.meats, function(val,i) {
         return $('<div/>', { class: 'form-check form-check-inline' }).html([
-            $('<input/>', { class: "btn-check", type: "checkbox", id: "meatCheckbox"+i, value: val }).text(val),
-            $('<label/>', { class: "btn btn-primary topping", for: "meatCheckBox"+i }).text(val)]
+            $('<input/>', { class: "btn-check", type: "checkbox", autocomplete: "off", id: "meatCheckbox"+i, value: val }),
+            $('<label/>', { class: "btn btn-outline-primary topping", for: "meatCheckBox"+i }).text(val)]
         )
       
     })
@@ -56,9 +56,87 @@ $('#meat_choices').html(
 $('#vegetable_choices').html(
     $.map(toppings.vegetables, function(val,i) {
         return $('<div/>', { class: 'form-check form-check-inline' }).html([
-            $('<input/>', { class: "btn-check", type: "checkbox", id: "vegetableCheckbox"+i, value: val }).text(val),
-            $('<label/>', { class: "btn btn-primary topping", for: "vegetableCheckBox"+i }).text(val)]
+            $('<input/>', { class: "btn-check", type: "checkbox", autocomplete: "off", id: "vegetableCheckbox"+i, value: val }),
+            $('<label/>', { class: "btn btn-outline-primary topping", for: "vegetableCheckBox"+i }).text(val)]
         )
       
     })
 );
+
+$(document).ready(function() {
+    $("input#sauceCheckbox0").prop({'checked': true})
+    $("input#cheeseCheckbox0").prop({'checked': true})
+    $("span#pizzaTotal").text("$" + ($(":input:checked").length * 1.5))
+})
+
+$(document).ready(function() {
+    $("label[class*='btn-outline']").click(function() {
+
+        var input = $(this).siblings('input')
+        input.prop({'checked': !input.prop('checked')})
+
+        updatePrice()
+    })
+})
+
+$(document).ready(function(){
+    $("button#savePizza").click(function() {
+        addPizza()
+    })
+})
+
+function updatePrice() {
+    $("span#pizzaTotal").text("$" + ($(":input:checked").length * 1.5))
+}
+
+var pizzas = []
+
+function addPizza() {
+
+    var pizza_toAdd = {
+        "toppings": [],
+        "price": $(":input:checked").length * 1.5
+    }
+
+    
+
+    $(":input:checked").each(function(index, element) {
+        pizza_toAdd.toppings.push(element.value)
+    })
+
+    pizzas.push(pizza_toAdd)
+
+    reset_toppings()
+
+    showPizzas()
+};
+
+function reset_toppings() {
+    $(":input:checked").each(function() {
+        if ($(this)[0].id == "sauceCheckbox0" || $(this)[0].id == "cheeseCheckbox0") {
+            $(this).prop("checked", true)
+        } else {
+            $(this).prop("checked", false)
+        }
+    })
+    
+}
+
+
+function showPizzas() {
+    $("#pizzaList").html(
+        $.map(pizzas, function(pizzas) {
+            console.log(pizzas)
+            return $("<div/>").html([
+                $("<ul/>", { class: "list-group" }).html(
+                    $.map(pizzas.toppings, function(topping){
+                        return $("<li/>", { class: "list-group-item" }).text(topping)
+                    })
+                ),
+                $("<span/>").text("Pizza Cost: $" + pizzas.price),
+                $("<br/>")
+
+            ])
+        })
+    )
+}
